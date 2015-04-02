97e33981926e4475da1b7b9f7af2d1ddb8cacf9f
Richard Kerr <richard.m.kerr@vanderbilt.edu>
2015-04-02 19:48:00 +0000

    Modified Scheduler to clarify purposes of methods, include additional data for simulating simultaneous execution.

--- Scheduler.hxx
+++ Scheduler.hxx
@@ -8,16 +8,22 @@
 class Scheduler
 {
     public:
-        Scheduler();
+        Scheduler(Schedule*) : schedule(Schedule) {};
         ~Scheduler();
+        
         std::queue<Task> getTaskQueue();
+        
+        //Increment time passed, unload+log finished tasks 
         void updateTasks(int timestep = 1);
+        
+        //Uses the Schedule strategy to order tasks
         void sortQueue();
 
 
     private:
         void logTask();
-
+        
+        size_t maxSimultaniousProcesses;//# of tasks to update simulataniously
         std::queue<Task> taskQueue;
         Schedule* schedule;
 };
